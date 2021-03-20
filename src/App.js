162178cdee94ccc7c2bff591c487c0d18f95a63a
papaga3 <ng.q.huy1008@gmail.com>
2021-03-20 14:53:00 +0200

    Inital Commit, Boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {hot} from "react-hot-loader/root";
+import "./App.css";
+
+const App = () => {
+    return(
+        <div className="App">
+            <h1> Hello World! </h1>
+            <h2> Nova Lina </h2>
+        </div>
+    );
+}
+
+export default hot(App);
