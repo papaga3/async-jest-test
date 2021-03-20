e37fa78739c81dca6821bb3371b8fc6c5ce9d0dc
papaga3 <ng.q.huy1008@gmail.com>
2021-03-20 15:02:22 +0200

    comment on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,19 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    entry: ["react-hot-loader/patch", "./src/index.js"],
+    entry: ["react-hot-loader/patch", "./src/index.js"], // Entry point of the application
     mode: "development",
     module: {
         rules: [
             {
+                // Loader for js and jsx files
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: "babel-loader",
                 options: {presets: ["@babel/env"]}
             },
             {
+                // Loader for css files
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
             }
@@ -21,14 +23,17 @@ module.exports = {
     },
     resolve: { 
         extensions: ["*", ".js", ".jsx"],
+        // Set alias for hot reloading react-dom (can hot reload react hooks)
         alias: {"react-dom" : "@hot-loader/react-dom"}
     },
     output: {
+        // Output bundle file
         path: path.resolve(__dirname, "dist/"),
         publicPath: "/dist/",
         filename: "bundle.js"
     },
     devServer: {
+        // Dev server configuration
         contentBase: path.join(__dirname, "public/"),
         port: 3000,
         publicPath: "http://localhost:3000/dist/",
